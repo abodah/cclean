5d235179e90191ddc16773cc9c1efff98f1908d1
Steve Ruben <stevetuenkam@gmail.com>
2019-10-25 01:42:07 +0100

    adding intro views WIP
    Signed-off-by: Steve Ruben <stevetuenkam@gmail.com>

---
+++ app/src/main/java/org/abodah/petit/cclean/CClean.java
@@ -0,0 +1,13 @@
+package org.abodah.petit.cclean;
+
+import android.app.Application;
+
+public class CClean extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
